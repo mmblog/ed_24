fd083dde3712a64e3d71e2327ab008eb21ed4287
josemanuelcarmona <152870185+josemanuelcarmona@users.noreply.github.com>
2023-12-04 13:59:23 +0100

    Update HolaMundo.java
    añadida la variable num3

--- UD2/src/ies/torredelrey/es/HolaMundo.java
+++ UD2/src/ies/torredelrey/es/HolaMundo.java
@@ -11,10 +11,11 @@ public class HolaMundo {
 		// Declarar una variable, 
 		int num1 = 5;
 		int num2 = 6;
+		int num3 = 1; //Nueva variable to wapa
 		
-		System.out.println("El valor de num1 es: "+num1+" y el valor de num2 es: "+num2);
+		System.out.println("El valor de num1 es: "+num1+" el valor de num2 es: "+num2 "el valor de num2 es: "+num3); 
 		
-		int suma = num1 + num2;
+		int suma = num1 + num2 + num3; //nuevas variables metia to crema
 		System.out.println("El valor de la suma es: "+suma);
 		
 	
