fbde89dc4b5775bbeacbc354df6a613fb97b2d84
mariamartinbernal <mariamartinbernal@gmail.com>
2023-12-04 13:11:29 +0100

    Subida inicial
    Subida inicial

---
+++ UD2/src/ies/torredelrey/es/HolaMundo.java
@@ -0,0 +1,24 @@
+package ies.torredelrey.es;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo, este es mi primer programa");
+		System.out.println("Este es mi segundo mensaje");
+		
+		// Declarar una variable, 
+		int num1 = 5;
+		int num2 = 6;
+		
+		System.out.println("El valor de num1 es: "+num1+" y el valor de num2 es: "+num2);
+		
+		int suma = num1 + num2;
+		System.out.println("El valor de la suma es: "+suma);
+		
+	
+
+	}
+
+}
